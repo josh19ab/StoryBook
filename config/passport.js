b7e23a485c7868dfc5428c0bc54dd6394e771352
Joshua <joah19ab@gmail.com>
2024-04-10 01:44:37 +0530

    updated final

---
+++ config/passport.js
@@ -0,0 +1,54 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const User = require("../models/User");
+
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        try {
+          // Check if user already exists in our database
+          const existingUser = await User.findOne({ googleId: profile.id });
+
+          if (existingUser) {
+            // If user already exists, pass the existing user to done function
+            done(null, existingUser);
+          } else {
+            // If user doesn't exist, create a new user
+            const newUser = new User({
+              googleId: profile.id,
+              displayName: profile.displayName,
+              firstName: profile.name.givenName,
+              lastName: profile.name.familyName,
+              image: profile.photos[0].value,
+            });
+
+            await newUser.save();
+            done(null, newUser);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+};
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+const Story = require("../models/Story");
+
+// @desc Login / Landing Page
+// @route GET /
+
+router.get("/", ensureGuest, (req, res) => {
+  res.render("login", {
+    layout: "login",
+  });
+  console.log(req.user);
+});
+
+// @desc Dashboard
+// @route GET /dashboard
+
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  console.log(req.session);
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean()
+    console.log(stories);
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+module.exports = router;
