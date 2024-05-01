4e695bc4752f973b82b6ea036331aa43d4e1ef70
Joshua <joah19ab@gmail.com>
2024-05-01 13:04:42 +0530

    deploy fix

--- app.js
+++ app.js
@@ -92,6 +92,7 @@ app.use(function (req, res, next) {
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
+
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
 app.use("/stories", require("./routes/stories"));
