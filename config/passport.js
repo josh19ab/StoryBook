690e3dcbd266ba9c66375d80a66eebafa835a917
Joshua <joah19ab@gmail.com>
2024-06-03 11:18:35 +0530

    Deployment Changes

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL: "https://story-book-omega.vercel.app/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         // passport callback function
